2865a1715f5171ba8d17a6f3d9af450a96f0d70a
Nicoleta Tuns <ndt@uni-tel.dk>
2022-10-19 09:12:23 +0200

    upgraded storybook, added chromatic and deleted package-lock.json

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,20 +1,16 @@
-const path = require("path");
-const { mergeConfig } = require("vite");
+const path = require('path');
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: [
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
+    "@storybook/addon-interactions"
   ],
-  framework: {
-    name: "@storybook/vue3-vite",
-    options: {},
-  },
-  // async viteFinal(config, { configType }) {
-  //   console.log(config, configType);
-  //   return mergeConfig(config, {
-  //     cacheDir: "node_modules/.vite-storybook",
-  //   });
-  // },
-};
+  "framework": {
+    "name": "@storybook/vue3-vite",
+    "options": {}
+  }
+}
